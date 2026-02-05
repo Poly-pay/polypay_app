5a05665b0e33e93a999686faf045cc49c163faa6
BoHsuu <huygon764@gmail.com>
2026-02-05 17:57:36 +0700

    Add test service to seed data on staging

--- packages/backend/src/quest/quest.module.ts
+++ packages/backend/src/quest/quest.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { QuestController } from './quest.controller';
 import { QuestService } from './quest.service';
 import { QuestSeederService } from './quest-seeder.service';
+import { LeaderboardSeederService } from './leaderboard-seeder.service';
 
 @Module({
   imports: [QuestModule],
   controllers: [QuestController],
-  providers: [QuestService, QuestSeederService],
+  providers: [QuestService, QuestSeederService, LeaderboardSeederService], // TODO: remove leaderboard seed service later
   exports: [QuestService],
 })
 export class QuestModule {}
