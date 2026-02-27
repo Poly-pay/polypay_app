44813a0cfbe57aa20bf0260937c3362798d620ea
BoHsuu <huygon764@gmail.com>
2026-02-27 18:28:53 +0700

    Remove unused import for AccountChainIdMigrationService in quest.module.ts

--- packages/backend/src/quest/quest.module.ts
+++ packages/backend/src/quest/quest.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { QuestController } from './quest.controller';
 import { QuestService } from './quest.service';
 import { QuestSeederService } from './quest-seeder.service';
-import { AccountChainIdMigrationService } from './update-chain.service';
 
 @Module({
   imports: [QuestModule],
