f7cf61568dc45066d1543a278d71de6597b6d0c5
BoHsuu <115441679+Huygon764@users.noreply.github.com>
2026-02-27 18:31:37 +0700

    Remove dummy file (#176)
    * Remove dummy file
    
    * Remove unused import for AccountChainIdMigrationService in quest.module.ts

--- packages/backend/src/quest/quest.module.ts
+++ packages/backend/src/quest/quest.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { QuestController } from './quest.controller';
 import { QuestService } from './quest.service';
 import { QuestSeederService } from './quest-seeder.service';
-import { AccountChainIdMigrationService } from './update-chain.service';
 
 @Module({
   imports: [QuestModule],
   controllers: [QuestController],
-  providers: [QuestService, QuestSeederService, AccountChainIdMigrationService],
+  providers: [QuestService, QuestSeederService],
   exports: [QuestService],
 })
 export class QuestModule {}
