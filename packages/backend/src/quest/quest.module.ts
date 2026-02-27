d2796bf906a396c6c697f61a8dda2a6dbe2e3a9b
BoHsuu <huygon764@gmail.com>
2026-02-27 18:20:16 +0700

    Add service to update chainId from account to horizen testnet chainid

--- packages/backend/src/quest/quest.module.ts
+++ packages/backend/src/quest/quest.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { QuestController } from './quest.controller';
 import { QuestService } from './quest.service';
 import { QuestSeederService } from './quest-seeder.service';
+import { AccountChainIdMigrationService } from './update-chain.service';
 
 @Module({
   imports: [QuestModule],
   controllers: [QuestController],
-  providers: [QuestService, QuestSeederService],
+  providers: [QuestService, QuestSeederService, AccountChainIdMigrationService],
   exports: [QuestService],
 })
 export class QuestModule {}
